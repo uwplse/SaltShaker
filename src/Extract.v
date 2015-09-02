a4dc3fe8875ca31a9fcc26e7d3b378c6bb190655
Konstantin Weitz <weitzkon@cs.uw.edu>
2015-09-01 19:41:43 -0700

    Initializing machine state

--- src/Extract.v
+++ src/Extract.v
@@ -1,91 +1,69 @@
 Require Import X86Semantics.
 Import X86_RTL.
 Import X86_Compile.
-
-Check instr_to_rtl.
-
-Check interp_rtl.
-
-Print RTL.
-
-Check step.  (* appears to be execution *)
-
-Print  RTL.
-
-
-Check steps.
-
-Print rtl_state.
-
-Check instr_to_rtl.
-
-(* seee simulator/test.ml *)
-
+Import X86_MACHINE.
+Require Import Maps.
+Import PTree.
 Require Import Bits.
 
-Check Word.zero.
-
-Check Word.int.
+(* initialize state, inspired by see simulator/test.ml *)
 
 Definition bii := id (A:=nat).
 
-Require Import Maps.
+Definition empty_mem : AddrMap.t int8 := (@Word.zero (bii 7), PTree.empty _).
+Definition empty_reg : fmap register int32 := (fun reg => @Word.zero (bii 31)).
+Definition empty_seg : fmap segment_register int32 := (fun seg => @Word.zero (bii 31)).
+Definition pc := @Word.zero (bii 31).
 
-Import PTree.
+Definition empty_oracle : oracle.
+  refine {|
+    oracle_bits := (fun a b => @Word.zero (bii a)); (* originally: zero_big_int) *)
+    oracle_offset := 0 (* originally: zero_big_int  *)
+  |}.
+Defined.
 
-Definition empty_mem := (@Word.zero (bii 7), PTree.empty).
-Definition empty_reg := (fun reg => @Word.zero (bii 31)).
-Definition empty_seg := (fun seg => @Word.zero (bii 31)).
-Definition empty_regpcseg = (empty_reg, Word.zero (bii 31), empty_seg);;
-
-let empty_oracle =
-  { oracle_bits = (fun a b -> zero_big_int); oracle_offset = zero_big_int }
-
-let (loaded_mem, _) = load_memory empty_mem [] mem_file;;
-let (loaded_reg, pc, loaded_seg) = load_regpc empty_regpcseg reg_file;;
-
-let init_machine =     
-  { gp_regs = loaded_reg;
-    seg_regs_starts =  loaded_seg;
-    seg_regs_limits = (fun seg_reg->(Word.repr (bii 31) (Word.max_unsigned
+Definition init_machine : core_state. 
+  refine {|
+    gp_regs := empty_reg;
+    seg_regs_starts := empty_seg;
+    seg_regs_limits := (fun seg_reg=>(@Word.repr (bii 31) (Word.max_unsigned
                                                            (bii 31))));
-    flags_reg = (fun f -> Word.zero (bii 0));
-    control_regs = (fun c -> Word.zero (bii 31));
-    debug_regs =  (fun d -> Word.zero (bii 31));
-    pc_reg = pc;
-  };;
+    flags_reg := (fun f => @Word.zero (bii 0));
+    control_regs := (fun c => @Word.zero (bii 31));
+    debug_regs :=  (fun d => @Word.zero (bii 31));
+    pc_reg := pc
+  |}.
+Defined.
 
+Definition empty_fpu_machine : fpu_state.
+refine {|
+  fpu_data_regs := (fun fpr => (* @Word.repr(bii 2) *) (@Word.zero(bii 79)));
+  fpu_status := @Word.zero(bii 15);
+  fpu_control := @Word.zero(bii 15);
+  fpu_tags := (fun t => (* @Word.repr(bii 2) *) (@Word.zero(bii 1)));
+  fpu_lastInstrPtr := @Word.zero(bii 47);
+  fpu_lastDataPtr := @Word.zero(bii 47);
+  fpu_lastOpcode := @Word.zero(bii 10) (* originally: (bii 2) *)
+|}.
+Defined.
 
-(*let init_fpu_machine = 
-{
-  This is not the syntax, just taken from x86semantics
-  { fpu_data_regs : (int3, int80) fmap; fpu_status : 
-                     int16; fpu_control : int16;
-                     fpu_tags : (int3, int2) fmap; fpu_lastInstrPtr : 
-                     int48; fpu_lastDataPtr : int48; fpu_lastOpcode : 
-                     RTL.int }
-};;
-*)
+Definition init_full_machine : mach_state.
+  refine {|
+   core := init_machine;
+   fpu := empty_fpu_machine
+  |}.
+Defined.
 
-let empty_fpu_machine =
-{
-  fpu_data_regs = (fun fpr -> (Word.repr(bii 2) (Word.zero(bii 79))));
-  fpu_status = Word.zero(bii 15);
-  fpu_control = Word.zero(bii 15);
-  fpu_tags = (fun t -> (Word.repr(bii 2) (Word.zero(bii 1))));
-  fpu_lastInstrPtr = Word.zero(bii 47);
-  fpu_lastDataPtr = Word.zero(bii 47);
-  fpu_lastOpcode = Word.zero(bii 2);
-};;
+Definition init_rtl_state : rtl_state.
+  refine {|
+    rtl_oracle := empty_oracle;
+    rtl_mach_state := init_full_machine;
+    rtl_memory := empty_mem
+  |}.
+Defined.
 
-let init_full_machine = 
-{  core = init_machine;
-   fpu = empty_fpu_machine;
-}
+Check instr_to_rtl.
+Check interp_rtl.
+Print RTL.
 
-let init_rtl_state =
-  { rtl_oracle = empty_oracle;
-    rtl_mach_state = init_full_machine; 
-    rtl_memory = loaded_mem 
-  };; 
-
+Print rtl_instr.
