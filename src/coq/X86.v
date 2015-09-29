dd10d6c236c716cf43148e36f659892fb09102f4
Konstantin Weitz <weitzkon@cs.uw.edu>
2015-09-29 14:54:09 -0700

    Made build easier

--- src/coq/X86.v
+++ src/coq/X86.v
@@ -8,6 +8,9 @@ Require Import Bits.
 Require Import List.
 Import ListNotations.
 Require Import SpaceSearch.
+Require Import Rosette.
+Import BinNums.
+Import Word.
 
 (* initialize state, inspired by see simulator/test.ml *)
 
@@ -105,3 +108,57 @@ Definition run p := RTL_step_list p init_rtl_state.
 Definition four_plus_six :=
   let s := run (add four six) in
     (fst s, gp_regs (core (rtl_mach_state (snd s))) EAX).
+
+Extraction Language Scheme.
+
+(* Existing Instance listSpaceSearch. *)
+Existing Instance rosette.
+
+Definition threehundred : int32.
+  compute.
+  refine {| Word.intval := 300 |}.
+  compute.
+  intuition congruence.
+Defined.
+
+Definition zero : int32.
+  refine {| Word.intval := 0 |}.
+  compute.
+  intuition congruence.
+Defined.
+
+Fixpoint positives (n:nat) : Space positive.
+  refine (match n with
+  | 0 => single xH
+  | S n => let i := positives n in _
+  end).
+  refine (union (single xH) _).
+  refine (bind i (fun x => (union (single (xI x)) (single (xO x))))).
+Defined.
+
+Axiom ADMIT : forall A, A.
+
+Definition int32s (n:nat) : Space int32.
+  refine (union (single zero) _).
+  refine (bind (positives n) (fun x => _)).
+  refine (single {| intval := Zpos x |}).
+  intuition.
+  apply ADMIT.
+Defined.
+
+Definition verification : option (int32 * int32 * (RTL_ans unit + int32)).
+  refine (let p := 0 in _).
+  refine (search _).
+  refine (bind (int32s p) (fun n => _)).
+  refine (bind (int32s p) (fun m => _)).
+  refine (let s := run (add n m) in _).
+  refine (match (fst s) with 
+  | Okay_ans _ => _
+  | a => single (n,m,inl a)
+  end).
+  refine (let r := gp_regs (core (rtl_mach_state (snd s))) EAX in _).
+  refine (if Word.eq (Word.add n m) r then empty else single (n,m,inr r)).
+Defined.
+
+(* they use the stdlib module ExtrOcamlZBigInt to extract Z to Ocaml's big_int *)
+Extraction "x86sem" verification.
