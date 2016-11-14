6eed64ebf625ebb2fe2c87654ad6f881382031e2
Konstantin Weitz <weitzkon@uw.edu>
2016-11-14 08:12:40 -0800

    update eval script

--- eval/eval.sh
+++ eval/eval.sh
@@ -2,12 +2,23 @@
 
 cd /x86sem/eval
 
-segments=( 'xab' 'xac' 'xad' 'xae' 'xaf' 'xag' 'xah'
+segments=( 'xaa' 'xab' 'xac' 'xad' 'xae' 'xaf' 'xag' 'xah'
            'xai' 'xaj' 'xak' 'xal' 'xam' 'xan' 'xao' )
 
-cat xaa > next
-../src/racket/compareAll.rkt next
-cat xaa > explored
+# segments=( 'yaa' 'yab' 'yac' 'yad' 'yae' 'yaf' 'yag' 'yah'
+#            'yai' 'yaj' 'yak' 'yal' 'yam' 'yan' 'yao' )
+
+echo "================[individual]=============================="
+
+for i in "${segments[@]}"
+do
+    ../src/racket/compareAllInc.rkt $i /dev/null
+done
+
+echo "================[incremental]=============================="
+
+rm -rf next explored
+touch explored
 
 for i in "${segments[@]}"
 do
@@ -16,5 +27,18 @@ do
     cat $i >> explored
 done
 
-rm next
-rm explored
+rm -rf next explored
+
+echo "================[everything]=============================="
+
+rm -rf next
+touch next
+
+for i in "${segments[@]}"
+do
+    cat $i >> next
+    ../src/racket/compareAllInc.rkt next /dev/null
+done
+
+rm -rf next
+
